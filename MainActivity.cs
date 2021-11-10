54604af39e454c5edce099e85513655258075663
Setsu-Hanatsuki <66735232+Setsu-Hanatsuki@users.noreply.github.com>
2021-11-10 16:31:41 +0900

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,55 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Webkit;
+using AndroidX.AppCompat.App;
+using Java.Interop;
+using Java.Lang;
+
+namespace App3
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            WebView web_view = FindViewById<WebView>(Resource.Id.webview);
+            web_view = FindViewById<WebView>(Resource.Id.webview);
+            web_view.Settings.JavaScriptEnabled = true;
+            web_view.Settings.DomStorageEnabled = true;
+            web_view.SetWebViewClient(new HelloWebViewClient());
+            web_view.LoadUrl("http://10.0.2.2:5000");
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private class HelloWebViewClient : WebViewClient
+        {
+            public override bool ShouldOverrideUrlLoading(WebView view, IWebResourceRequest request)
+            {
+                view.LoadUrl(request.Url.ToString());
+                return false;
+            }
+        }
+        /*
+        private class sampleClass : Object
+        {
+            [Export]
+            [JavascriptInterface]
+            public void Test_001()
+            {
+                //本来はこれがデバッグに表示されるはず....↓
+                System.Diagnostics.Debug.WriteLine("あれこれテストしてみたが.....");
+            }
+        }
+        */
+    }
+}
