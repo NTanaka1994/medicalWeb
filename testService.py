5289d866271816ed5c44397bce7884d9ab054de0
Setsu-Hanatsuki <66735232+Setsu-Hanatsuki@users.noreply.github.com>
2021-11-24 08:44:50 +0900

    Update testService.py

--- testService.py
+++ testService.py
@@ -1437,4 +1437,4 @@ def apply_caching(response):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0")#,ssl_context=('openssl/server.crt', 'openssl/server.key'))
